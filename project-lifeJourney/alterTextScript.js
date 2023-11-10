80376c93da9f34b7ab72817b1ba33feafd7e0885
rohitsinha969 <102020637+rohitsinha969@users.noreply.github.com>
2023-11-10 20:33:33 +0530

    gitignore

---
+++ project-lifeJourney/alterTextScript.js
@@ -0,0 +1,34 @@
+const textContainer = document.getElementById("text");
+const textArray = ["a social homosapien ", "a developer by profession", "an artist by creativity", "a photographer by passion"]; // Replace with your desired designations
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeText() {
+    const currentText = textArray[textIndex];
+    if (isDeleting) {
+        textContainer.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        textContainer.textContent = currentText.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    const typingSpeed = isDeleting ? 35 : 50; // Adjust typing and deleting speed as needed
+
+    if (!isDeleting && charIndex === currentText.length) {
+        isDeleting = true;
+        setTimeout(typeText, 1000); // Delay before starting to delete
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        textIndex = (textIndex + 1) % textArray.length;
+        setTimeout(typeText, 500); // Delay before typing next text
+    } else {
+        setTimeout(typeText, typingSpeed);
+    }
+}
+
+// Start typing
+typeText();
+
+
---
+++ project-lifeJourney/imageFlipScript.js
@@ -0,0 +1,13 @@
+// Array of image sources
+var images = ['/project-lifeJourney/images/img1.png', '/project-lifeJourney/images/img2.png', '/project-lifeJourney/images/img3.png', '/project-lifeJourney/images/img4.png'];
+var currentIndex = 0;
+
+// Function to change the image
+function changeImage() {
+    var imageContainer = document.getElementById('image-container');
+    imageContainer.innerHTML = '<img src="' + images[currentIndex] + '" alt="Image ' + (currentIndex + 1) + '">';
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Set interval to change image every 2 seconds
+setInterval(changeImage, 2500);
