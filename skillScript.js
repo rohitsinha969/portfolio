4c99a93f98469dcaa86d29079be513ae941e044b
rohitsinha969 <102020637+rohitsinha969@users.noreply.github.com>
2023-11-13 06:35:47 +0530

    skill-commit-not-final-color-changes-req

--- skillScript.js
+++ skillScript.js
@@ -1,33 +1,80 @@
-const progressCircles = document.querySelectorAll('.progress-circle');
-        const percentTexts = document.querySelectorAll('.percent-text');
-        const circumference = 2 * Math.PI * 60;
+// dummy button rotation and section onclick script
+const sections = document.querySelectorAll(".accordion-section");
 
-        // function updateProgressFromText() {
-        //     const textContent = percentText.textContent;
-        //     const percentage = parseFloat(textContent); // Extract the numerical value
-        //     const strokeDasharrayValue = (percentage / 100) * circumference;
-        //     progressCircle.style.animationDuration = '2s';
-        //     progressCircle.style.strokeDasharray = `0, ${circumference}`;
+sections.forEach(section => {
+    section.addEventListener("click", () => {
+        section.classList.toggle("active");
+        const button = section.querySelector(".dummy-button");
+        button.classList.toggle("rotated");
+    });
+});
 
-        //     setTimeout(() => {
-        //         progressCircle.style.strokeDasharray = `${strokeDasharrayValue}, ${circumference}`;
-        //     }, 100);
-        // }
-        function updateProgressFromText() {
-            progressCircles.forEach((progressCircle, index) => {
-                const textContent = percentTexts[index].textContent;
-                const percentage = parseFloat(textContent); // Extract the numerical value
-                const radius = 60; // Updated radius
-                const circumference = 2 * Math.PI * radius; // Updated circumference calculation
-                const strokeDasharrayValue = (percentage / 100) * circumference;
-                progressCircle.style.animationDuration = '2s';
-                progressCircle.style.strokeDasharray = `0, ${circumference}`;
 
-                setTimeout(() => {
-                    progressCircle.style.strokeDasharray = `${strokeDasharrayValue}, ${circumference}`;
-                }, 100);
-            });
+
+//  output palate script
+document.querySelectorAll(".run-code").forEach(button => {
+    button.addEventListener("click", () => {
+        const section = button.getAttribute("data-section");
+        const outputPalate = document.getElementById(`output-${section}`);
+        let texts = [];
+
+        if (section === "1") {
+            texts = [
+                "// Frontend Development \u{12810}",
+                "\xa0\xa0\xa0HTML",
+                "\xa0\xa0\xa0CSS",
+                "\xa0\xa0\xa0Javascript - DOM Manipulation/Advanced Js",
+                "\xa0\xa0\xa0JSON",
+                // Add more lines as needed for section 1
+            ];
+        } else if (section === "2") {
+            texts = [
+                "// Backend Development \u{EA105}",
+                "\xa0\xa0\xa0Java - Advanced Java",
+                "\xa0\xa0\xa0Collection Framework",
+                "\xa0\xa0\xa0C++",
+                "\xa0\xa0\xa0Node Js",
+                // Add more lines as needed for section 2
+            ];
+        } else if (section === "3") {
+            texts = [
+                "// Database Development \u{DB105}",
+                "\xa0\xa0\xa0SQL",
+                "\xa0\xa0\xa0MySQL - Workbench/Shell",
+                "\xa0\xa0\xa0MongoDB",
+                // Add more lines as needed for section 2
+            ];
+        } else if (section === "4") {
+            texts = [
+                "// Data/Algorithmic Development \u{DB105}",
+                "\xa0\xa0\xa0Data Structure Designing",
+                "\xa0\xa0\xa0Algorithmic Development",
+                "\xa0\xa0\xa0Algorithmic Implementation/Analysis",
+                "\xa0\xa0\xa0Dynamic Data Management",
+                // Add more lines as needed for section 2
+            ];
+        } else if (section === "5") {
+            texts = [
+                "// Interpersonal/Soft Skills \u{DB105}",
+                "\xa0\xa0\xa0Project Development",
+                "\xa0\xa0\xa0Team Building/Strategy Planning",
+                "\xa0\xa0\xa0Adaptibility to competetive situations",
+                // Add more lines as needed for section 2
+            ];
         }
 
-        // Call the function to update the progress based on the value in the SVG text
-        updateProgressFromText();
+        outputPalate.innerHTML = ""; // Clear previous content
+        let lineIndex = 0;
+
+        const typeText = () => {
+            if (lineIndex < texts.length) {
+                const text = texts[lineIndex];
+                outputPalate.innerHTML += text + "<br>";
+                lineIndex++;
+                setTimeout(typeText, 1000); // Adjust the typing speed (milliseconds)
+            }
+        };
+
+        typeText();
+    });
+});
